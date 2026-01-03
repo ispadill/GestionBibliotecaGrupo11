7b7bcba410318a1fd1e6a7296df1f92b8bec7eaa
frbartol <frbartol@unirioja.es>
2026-01-03 14:35:34 +0100

    logica negocio empezada

---
+++ LogicaNegocio/LNPersonalBiblioteca.cs
@@ -0,0 +1,29 @@
+﻿using ClassLibrary1;
+using Persistencia.CRUD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio
+{
+    public abstract class LNPersonalBiblioteca
+    {
+        protected PersonalBiblioteca p;
+        
+        public Usuario BuscarUsuario(string dni)
+        {
+            return PersistenciaUsuario.READ(dni);
+        }
+        public List<Usuario> ListarUsuarios()
+        {
+            return PersistenciaUsuario.READALL();
+        }
+        public static LNPersonalBiblioteca IniciarSesion(string usuario, string contra)
+        {
+            PersonalBiblioteca p = PersistenciaPersonal.LOGIN(usuario, contra);
+            return p;
+        }
+    }
+}
